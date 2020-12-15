2e6322c8c81a21731ecc9d1abee67cee06718c57
Steffan <s194006@student.dtu.dk>
2020-12-15 10:39:59 +0100

    Update

---
+++ src/Hangman.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+set JAVA_HOME="C:\Program Files (x86)\Java\jre1.8.0_221\bin";
+
+javac com\company\Hangman.java
+
+java com.company.Hangman
--- src/com/company/Hangman.java
+++ src/com/company/Hangman.java
@@ -1,9 +1,6 @@
 package com.company;
-import java.io.File;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.SQLOutput;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -13,34 +10,38 @@ public class Hangman {
     static List<Character> chars = new ArrayList<>();
     static String word;
     static int count;
-    static final int LIVES = 5;
-
+    static final int LIVES = 6;
+    // Static method that prints Hangman game
     public static void print(){
+        // Prints player's remaining lives
         System.out.println("Lives: " + (LIVES - count));
+        // Displays list of chars guessed by the player
         chars.forEach(ch -> System.out.print(ch + " "));
+        // Prints Hangman for different values of count
         System.out.println();
-        System.out.println("+--------+");
-        System.out.println("|        |");
-        System.out.print("|");
-        if (count > 0) System.out.print(String.format("%9s", "o"));
-        System.out.println();
-        System.out.print("|");
-        if (count > 1) System.out.print(String.format("%10s", "/|\\"));
+        System.out.println("+----------+");
+        System.out.println("| /        |");
+        System.out.print("|/");
+        if (count > 0) System.out.print(String.format("%10s", "O"));
         System.out.println();
         System.out.print("|");
-        if (count > 2) System.out.print(String.format("%9s", "|"));
+        if (count > 1) System.out.print(String.format("%10s", "/"));
+        if (count > 2) System.out.print("|");
+        if (count > 3) System.out.print("\\");
         System.out.println();
         System.out.print("|");
-        if (count > 3) System.out.print(String.format("%9s", "/ "));
-        if (count > 4) System.out.print("\\");
+        if (count > 4) System.out.print(String.format("%10s", "/"));
+        if (count > 5) System.out.print(" \\");
         System.out.println();
         System.out.println("|");
-        System.out.println("============");
+        System.out.println("==============");
         System.out.println();
-        if (count >= 5){
+        // Prints entire word if count is equal to 5
+        if (count == LIVES){
             for (char ch : word.toCharArray()){
                 System.out.print(ch + " ");
             }
+        // Prints guessed characters in word if count is less than 5
         } else {
             for (char ch : word.toCharArray()) {
                 if (chars.contains(ch)) {
@@ -52,8 +53,9 @@ public class Hangman {
             }
         }
         System.out.println();
-        // Prints hint character if count is equal to 5
-        if (count == 5){
+        // Prints hint character if count is equal to 4 and less than
+        // half of characters guessed.
+        if (count == LIVES - 1 && rem() > word.length() / 2){
             for (int i = 0; i < word.length(); i++){
                 if (!chars.contains(word.charAt(i))){
                     System.out.println("Hint: " + word.charAt(i));
@@ -62,7 +64,15 @@ public class Hangman {
             }
         }
     }
-
+    // Static method that returns the remaining number of characters
+    public static int rem(){
+        int rem = word.length();
+        for (int i = 0; i < word.length(); i++){
+            if (chars.contains(word.charAt(i))) rem--;
+        }
+        return rem;
+    }
+    // Static method that checks if the word contains the guessed character
     public static void check(char ch){
         if (chars.contains(ch)) {
             return;
@@ -71,11 +81,10 @@ public class Hangman {
         }
         chars.add(ch);
     }
-
+    // Loads words from different categories into list of Strings
     private static List<String> getWords(int n){
         List<String> _words = new ArrayList<String>();
         String fileName = "C:\\Projects\\Hangman\\src\\com\\company\\";
-        //String fileName = (n == 1)? "C:\\Projects\\src\\com\\company\\movies" : "com/company/celebrities";
         if (n == 1)
             fileName += "celebrities";
         else if (n == 2)
@@ -89,16 +98,28 @@ public class Hangman {
         }
         return _words;
     }
-
+    // Main method that simulates the Hangman game
     public static void main(String[] args) {
-        System.out.println("Hangman");
+        System.out.println("+-------------+");
+        System.out.println("|   Hangman   |");
+        System.out.println("+-------------+");
         for(; ;) {
+            words.clear();
             chars.clear();
             Scanner sc = new Scanner(System.in);
-            System.out.println("Select category: \n1 Celebrities \n2 Countries \n3 Movies");
+            System.out.println("Select category: \n1 Celebrities \n2 Countries \n3 Movies \n(Press X to exit game)");
             do {
-                int n = sc.nextInt();
-                words = getWords(n);
+                if (Character.toUpperCase(sc.next().charAt(0)) == 'X'){
+                    System.exit(0);
+                } else {
+                    try {
+                        int n = Integer.parseInt(sc.nextLine());
+                        words = getWords(n);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Please select a category");
+                        continue;
+                    }
+                }
             } while (words.isEmpty());
             Random random = new Random();
             word = (words.get(random.nextInt(words.size()))).toUpperCase();
